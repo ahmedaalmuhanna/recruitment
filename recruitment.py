217172690e70f095c2da89740001254b311867db
Ahmed <ahmed.a.almuhanna@hotmail.com>
2022-07-06 17:28:55 +0300

    My recruitment

--- recruitment.py
+++ recruitment.py
@@ -1,37 +1,83 @@
+myName =''
 def get_skills():
     # Add at least 3 random skills for the user to select from
-    return []
+    return ["Python","C++","Javascript","Juggling","Running","Eating","workout","swimming","video Games"]
 
 
 def show_skills(skills):
+    
+    for i, skill in enumerate(skills,1):
+        print(i, '. ' + skill, sep ='', end ='\n')
+        
+    # x=0
+    # while(x<len(skills)-1):
+    #     x = x+1
+    #     print( str(x) +". "+skills[x])
+        
+
+    
     # Pretty print skills to the user
     # Write your code here
     ...
 
 
 def get_user_skills(skills):
-    # Show the available skills and have user pick from them
-    # Write your code here
-    ...
+    show_skills(skills)
+    userSelections = []
+    
+    print("Select two skills")
+    firstSelection = input("Select the first skill:")
+    secondSelection = input("Select the second skill:")
+    userSelections.append(firstSelection)
+    userSelections.append(secondSelection)
 
+    return userSelections;
 
 def get_user_cv(skills):
+    cv ={}
+    cv["name"] = input("Your name\n")
+    
+    myName =cv["name"]
+    cv["age"] = input("Your age\n")
+    cv["years of experience"] = input("Your years of experience\n ")
+
+    cv["skills"] = get_user_skills(skills)
+    
+
+    print(cv["age"])
+    return cv
+    
+    
     # Get the users CV from their inputs
     # Write your code here
     ...
 
 
 def check_acceptance(cv, desired_skill):
-    # Check if the cv is acceptable or not and return a boolean based on that
-    # Write your code here
-    ...
+    # 
+    x = 0
 
+    print(cv['age'])
+    print(cv["years of experience"])
+    print(desired_skill in cv["skills"])
+    
+    if((int(cv['age']) >= 28 and int(cv['age'])<=40) and (int(cv["years of experience"]) > 3)) and(desired_skill in cv["skills"]):
+    #     for i in cv["skills"]:
+        return True
+    else:
+        return False
 
 def main():
-    # Write your main logic here by combining the functions above into the
-    # desired outcome
-    ...
+    print("Welcome to the special recruitment program, please answer the following questions:")
+    x = check_acceptance(get_user_cv(get_skills()), "C++")
+    print(myName)
 
+    if (x == True):
+        print(" you are accepted")
+    else:
+        print("you are not accepted")
+# print("sdsdsd")
+# get_user_skills(get_skills())
 
 if __name__ == "__main__":
     main()
